4870b55c067180bfce8ec74c73093d210fc3ccf5
gamagegit <cgamage92@yahoo.com>
2023-03-09 23:19:36 -0800

    160 solved

--- 160_insertion_two_linked_lists.py
+++ 160_insertion_two_linked_lists.py
@@ -6,28 +6,12 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        A_arr = []
-        B_arr = []
+        pA, pB = headA, headB
+        while pA != pB:
+            pA = headB if not pA else pA.next
+            pB = headA if not pB else pB.next
+        return pA
 
-        while headA:
-            A_arr.append(headA.val)
-            headA= headA.next
-        
-        while headB:
-            B_arr.append(headB.val)
-            headB= headB.next
-
-        mearge_point = 0
-        print(A_arr)
-        print(B_arr)
-
-        for i in range(1,len(A_arr)):
-            if A_arr[-i] == B_arr[-i]:
-                mearge_point = A_arr[-i]
-            else:
-                print(mearge_point)
-                return mearge_point
-        
         
         
         
